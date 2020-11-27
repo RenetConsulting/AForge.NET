72e81e6b9595decf0c894c9365a2ee38408bb946
aaltotsky <alex@renetusa.com>
2020-11-27 11:02:35 -0800

    Added VideoProperties console app

---
+++ Samples/Video/VideoProperties/Program.cs
@@ -0,0 +1,28 @@
+﻿using AForge.Video.DirectShow;
+using System;
+
+namespace VideoProperties
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var videoDevices = VideoDevices();
+            Console.WriteLine("Local capture devices");
+            foreach (FilterInfo device in videoDevices)
+            {
+                Console.WriteLine(device.Name);
+            }
+        }
+
+        public static FilterInfoCollection VideoDevices()
+        {
+            // show device list
+            // enumerate video devices
+            var videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            return videoDevices;
+        }
+    }
+}
